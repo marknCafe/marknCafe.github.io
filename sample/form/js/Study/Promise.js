753ae5db5ad4e546b9dfd449a2fb5dd27a3bc11c
marknCafe <markn.cafe@gmail.com>
2022-03-26 17:44:03 +0900

    Update Promise

--- sample/form/js/Study/Promise.js
+++ sample/form/js/Study/Promise.js
@@ -4,27 +4,44 @@ const MyPromise = function (executor) {
 }
 
 {
+    const genMyAggregateError = function (promises) {
+        const error = new Error('No Promise in MyPromise.any was resolved');
+        Object.defineProperty(error, 'errors', { value : [], writable: false, configurable : false});
+        const errors = error.errors;
+        promises.forEach(function (p) { errors.push(p._reason); });
+        return error;
+    };
+
     const organizeIntoArray = function (data) {
         if (data instanceof Array) { return data; }
+        const resArray =  Array();
         if (data instanceof Map) {
-            const resArray = Array();
             data.forEach(function (value, key) { resArray.push([key, value]); });
             return resArray;
         } else if (data instanceof Set) {
-            const resArray = Array();
             data.forEach(function (value) { resArray.push(value); });
             return resArray;
         } else if (data.split) {
             return data.split('');
+        } else if (data.length || data.size) {
+            try {
+                Array.prototype.forEach.call(data, function (value) { resArray.push(value); });
+                return resArray;
+            } catch (e) {
+                return undefined;
+            }
         }
         return undefined;
     }
     const genMyPromiseIterable = function (iterable, name, func) {
         const promises = organizeIntoArray(iterable);
         return new MyPromise(function (res, rej) {
-            if (promises instanceof Array == false) rej(new TypeError('Invalid Argument of "' + name + '"'));
+            if (promises instanceof Array == false) {
+                rej(new TypeError('Invalid Argument of "' + name + '"'));
+                return;
+            }
             const iid = setInterval(function () {
-                func(promises, iid, res, rej);
+                    func(promises, iid, res, rej);
             }, 0);
         });
     }
@@ -32,17 +49,21 @@ const MyPromise = function (executor) {
         return genMyPromiseIterable(iterable, 'MyPromise.All', function (promises, iid, res, rej) {
             for (let i = 0; i < promises.length; i ++) {
                 const p = promises[i];
-                if (p instanceof MyPromise == false) continue;
-                if (p._state == 'fulfilled') continue;
-                if (p._state == 'rejected') {
+                if (p instanceof MyPromise == false || p._state == 'fulfilled') {
+                    continue;
+                } else if (p._state == 'rejected') {
                     clearInterval(iid);
                     return rej(p._reason);
-                } else {
-                    return;
                 }
+                return;
             }
             clearInterval(iid);
-            return res(promises);
+
+            const values = [];
+            promises.forEach(function (p) {
+                values.push(p instanceof MyPromise ? p._value : p);
+            });
+            return res(values);
         });
     };
     const genMyPromiseAllSettled  = function (iterable) {
@@ -53,7 +74,16 @@ const MyPromise = function (executor) {
                 if (p._state == 'pending') return;
             }
             clearInterval(iid);
-            return res(promises);
+
+            const values = [];
+            promises.forEach(function (p) {
+                if (p._state == 'fulfilled') {
+                    values.push({ status : p._state, value : p._value });
+                } else {
+                    values.push({ status : p._state, reason : p._reason });
+                }
+            });
+            return res(values);
         });
     };
     const genMyPromiseAny = function (iterable) {
@@ -67,7 +97,7 @@ const MyPromise = function (executor) {
                 } else if (p._state == 'pending') return;
             }
             clearInterval(iid);
-            return rej('error'); // 詳細を調べて調整する
+            return rej(genMyAggregateError(promises));
         });
     };
     const genMyPromiseRace = function (iterable) {
@@ -138,10 +168,18 @@ const MyPromise = function (executor) {
     };
     const genExecutorFuncThen = function (parentPromise, onFulfilled, onRejected) {
         return function () {
+            let value;
             if (parentPromise._state == 'fulfilled') {
-                return onFulfilled instanceof Function ? onFulfilled(parentPromise._value) : parentPromise._value;
+                value = onFulfilled instanceof Function ? onFulfilled(parentPromise._value) : parentPromise._value;
+                this._isTakeOver = true;
+                return value;
             } else {
-                return onRejected instanceof Function ? onRejected(parentPromise._reason) : parentPromise._reason;
+                if (onRejected instanceof Function) {
+                    value = onRejected(parentPromise._reason);
+                    this._isTakeOver = true;
+                    return value;
+                }
+                throw parentPromise._reason;
             }
         };
     };
@@ -193,8 +231,9 @@ const MyPromise = function (executor) {
         _genResolutionFunc : { value : genResolutionFunc },
         _setRejection : { value : setRejection },
         _setResolution : { value : setResolution },
-        _state : { value : 'pending', writable : true },
+        _state : { value : 'pending', writable : false },
         _value : { value : undefined, writable : false },
-        _reason : { value : undefined, writable : false }
+        _reason : { value : undefined, writable : false },
+        _isTakeOver : { value : false, writable : false }
     });
 }
