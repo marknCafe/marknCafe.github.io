3ddd486d0c9d32341763654e0c353afab336e6b3
marknCafe <markn.cafe@gmail.com>
2022-03-27 23:05:43 +0900

    Update Promise

--- sample/form/js/Study/Promise.js
+++ sample/form/js/Study/Promise.js
@@ -1,6 +1,7 @@
+'use strict';
 const MyPromise = function (executor) {
     if (executor instanceof Function == false) throw new TypeError(String(executor) + ' is not a function');
-    this._constructor(executor);
+    this._init(executor);
 }
 
 {
@@ -35,46 +36,49 @@ const MyPromise = function (executor) {
     }
     const genMyPromiseIterable = function (iterable, name, func) {
         const promises = organizeIntoArray(iterable);
+        promises.forEach(function (p) {
+            if (p instanceof MyPromise && p._hasChildren == false) {
+                Object.defineProperty(p, '_hasChildren', { writable : false, configurable : false, value : true });
+            }
+        });
         return new MyPromise(function (res, rej) {
             if (promises instanceof Array == false) {
-                rej(new TypeError('Invalid Argument of "' + name + '"'));
+                rej(new TypeError('Invalid argument of "' + name + '"'));
                 return;
             }
-            const iid = setInterval(function () {
-                    func(promises, iid, res, rej);
-            }, 0);
+            promises.forEach(function (p) {
+                if (p instanceof MyPromise == false) return;
+                addFulfilledList.call(p, function () {
+                    func(p, promises, res, rej);
+                });
+            });
         });
-    }
+    };
     const genMyPromiseAll = function (iterable) {
-        return genMyPromiseIterable(iterable, 'MyPromise.All', function (promises, iid, res, rej) {
-            for (let i = 0; i < promises.length; i ++) {
-                const p = promises[i];
-                if (p instanceof MyPromise == false || p._state == 'fulfilled') {
-                    continue;
-                } else if (p._state == 'rejected') {
-                    clearInterval(iid);
-                    return rej(p._reason);
-                }
-                return;
+        let isFulfilled = false;
+        return genMyPromiseIterable(iterable, 'MyPromise.all', function (p, promises, res, rej) {
+            if (isFulfilled) return;
+            if (p._state == 'rejected') {
+                isFulfilled = true;
+                return rej(p._reason);
             }
-            clearInterval(iid);
-
-            const values = [];
+            for (let i = 0; i < promises.length; i ++) {
+                if (promises[i]._state == 'pending') return;
+            }
+            isFulfilled = true;
             promises.forEach(function (p) {
                 values.push(p instanceof MyPromise ? p._value : p);
             });
-            return res(values);
+            res(values);
         });
     };
     const genMyPromiseAllSettled  = function (iterable) {
-        return genMyPromiseIterable(iterable, 'MyPromise.AllSettled', function (promises, iid, res, rej) {
+        return genMyPromiseIterable(iterable, 'MyPromise.allSettled', function (promise, promises, res, rej) {
             for (let i = 0; i < promises.length; i ++) {
                 const p = promises[i];
                 if (p instanceof MyPromise == false) continue;
                 if (p._state == 'pending') return;
             }
-            clearInterval(iid);
-
             const values = [];
             promises.forEach(function (p) {
                 if (p._state == 'fulfilled') {
@@ -83,35 +87,34 @@ const MyPromise = function (executor) {
                     values.push({ status : p._state, reason : p._reason });
                 }
             });
-            return res(values);
+            res(values);
         });
     };
     const genMyPromiseAny = function (iterable) {
-        return genMyPromiseIterable(iterable, 'MyPromise.Any', function (promises, iid, res, rej) {
+        let isFulfilled = false;
+        return genMyPromiseIterable(iterable, 'MyPromise.any', function (promise, promises, res, rej) {
+            if (isFulfilled) return;
+            if (promise._state == 'fulfilled') {
+                isFulfilled = true;
+                return res(promise._value);
+            }
             for (let i = 0; i < promises.length; i ++) {
                 const p = promises[i];
-                if (p instanceof MyPromise == false) continue;
-                if (p._state == 'fulfilled') {
-                    clearInterval(iid);
-                    return res(p._value);
-                } else if (p._state == 'pending') return;
+                if (p instanceof MyPromise == false || p._state == 'rejected') continue;
+                return;
             }
-            clearInterval(iid);
-            return rej(genMyAggregateError(promises));
+            rej(genMyAggregateError(promises));
         });
     };
     const genMyPromiseRace = function (iterable) {
-        return genMyPromiseIterable(iterable, 'MyPromise.Race', function (promises, iid, res, rej) {
-            for (let i = 0; i < promises.length; i ++) {
-                const p = promises[i];
-                if (p instanceof MyPromise == false) continue;
-                if (p._state == 'fulfilled') {
-                    clearInterval(iid);
-                    return res(p._value);
-                } else if (p._state == 'rejected') {
-                    clearInterval(iid);
-                    return rej(p._reason);
-                }
+        let isFulfilled = false;
+        return genMyPromiseIterable(iterable, 'MyPromise.race', function (p, promises, res, rej) {
+            if (isFulfilled) return;
+            isFulfilled = true;
+            if (p._state == 'fulfilled') {
+                res(p._value);
+            } else {
+                rej(p._reason);
             }
         });
     };
@@ -127,113 +130,122 @@ const MyPromise = function (executor) {
     };
 
     Object.defineProperties(MyPromise, {
-        all : { value : genMyPromiseAll },
-        allSettled : { value : genMyPromiseAllSettled },
-        any : { value : genMyPromiseAny },
-        race : { value : genMyPromiseRace },
-        reject : { value : genMyPromiseReject },
-        resolve : { value : genMyPromiseResolve }
+        all : { value : genMyPromiseAll, configurable : false },
+        allSettled : { value : genMyPromiseAllSettled, configurable : false },
+        any : { value : genMyPromiseAny, configurable : false },
+        race : { value : genMyPromiseRace, configurable : false },
+        reject : { value : genMyPromiseReject, configurable : false },
+        resolve : { value : genMyPromiseResolve, configurable : false }
     });
 
     const MyPromiseConstructor = function (executor) {
-        try {
-            executor(this._genResolutionFunc(), this._genRejectionFunc());
-        } catch (e) {
-            this._setRejection(e);
-        }
+        onfulfilledList.set(this, []);
+        //Object.seal(this);
+        const promise = this;
+        const tid = setTimeout(function () {
+            clearTimeout(tid);
+            try {
+                executor(genResolutionFunc.call(promise), genRejectionFunc.call(promise));
+            } catch (e) {
+                setRejection.call(promise, e);
+            }
+        });
     };
     const MyPromiseCatch = function (onRejected) {
         return this.then(undefined, onRejected);
     }
     const genExecutor = function (parentPromise, func) {
         return function (res, rej) {
-            const iid = setInterval(function () {
-                const state = parentPromise._state;
-                if (state == 'pending') return;
-                clearInterval(iid);
-                try {
-                    res(func());
-                } catch (e) {
-                    rej(e);
-                }
-            }, 0);        
+            addFulfilledList.call(parentPromise, function () {
+                try { res(func()); }
+                catch (e) { rej(e); }
+            });
         };
     };
-    const genExecutorFinally = function (parentPromise, onFinally) {;
-        return genExecutor(parentPromise, onFinally);
-    };
     const MyPromiseFinally = function (onFinally) {
-        const newPromise = new MyPromise(genExecutorFinally(this, onFinally));
-        return newPromise;
+        return new MyPromise(genExecutor(this, onFinally));
     };
-    const genExecutorFuncThen = function (parentPromise, onFulfilled, onRejected) {
-        return function () {
+    const MyPromiseThen = function (onFulfilled, onRejected) {
+        if (this._hasChildren == false) {
+            Object.defineProperty(this, '_hasChildren', { writable : false, configurable : false, value : true });
+        }
+        if (this._throwTid) {
+            clearTimeout(this._throwTid);
+        }
+        const parentPromise = this;
+        const executor = genExecutor(this, function () {
             let value;
             if (parentPromise._state == 'fulfilled') {
                 value = onFulfilled instanceof Function ? onFulfilled(parentPromise._value) : parentPromise._value;
-                this._isTakeOver = true;
                 return value;
             } else {
                 if (onRejected instanceof Function) {
                     value = onRejected(parentPromise._reason);
-                    this._isTakeOver = true;
                     return value;
                 }
                 throw parentPromise._reason;
             }
-        };
-    };
-    const genExecutorThen = function (parentPromise, onFulfilled, onRejected) {
-        return genExecutor(parentPromise, genExecutorFuncThen(parentPromise, onFulfilled, onRejected));
-    };
-    const MyPromiseThen = function (onFulfilled, onRejected) {
-        const newPromise = new MyPromise(genExecutorThen(this, onFulfilled, onRejected));
-        return newPromise;
+        });
+        return new MyPromise(executor);
     };
     const genRejectionFunc = function () {
         const promise = this;
         return function (reason) {
-            promise._setRejection(reason);
+            const tid = setTimeout(function () {
+                clearTimeout(promise._throwTid);
+                if (promise._hasChildren) return;
+                throw '(in promise) ' + promise._reason;
+            });
+            Object.defineProperty(promise, '_throwTid', { writable : false, value : tid, configurable : false });
+            setRejection.call(promise, reason);
         };
     };
     const genResolutionFunc = function () {
         const promise = this;
         return function (value) {
-            promise._setResolution(value);
+            setResolution.call(promise, value);
         };
     }
+    const onfulfilled = function () {
+        onfulfilledList.get(this).forEach(function (func) {
+            func();
+        });
+        onfulfilledList.delete(this);
+    };
+    const addFulfilledList = function (func) {
+        if (this._state == 'pending') {
+            onfulfilledList.get(this).push(func);
+        } else {
+            func();
+        }
+    };
     const setRejection = function (reason) {
         if (this._state != 'pending') return;
-        Object.defineProperty(this, '_state', {writable : true, configurable : true});
-        this._state = 'rejected';
-        Object.defineProperty(this, '_state', {writable : false, configurable : false});
-        Object.defineProperty(this, '_reason', {writable : true, configurable : true});
-        this._reason = reason;
-        Object.defineProperty(this, '_reason', {writable : false, configurable : false});
+        Object.defineProperty(this, '_state', {writable : false, configurable : false, value : 'rejected'});
+        Object.defineProperty(this, '_reason', {writable : false, configurable : false, value : reason });
+        onfulfilled.call(this);
     };
     const setResolution = function (value) {
         if (this._state != 'pending') return;
-        Object.defineProperty(this, '_state', {writable : true, configurable : true});
-        this._state = 'fulfilled';
-        Object.defineProperty(this, '_state', {writable : false, configurable : false});
-        Object.defineProperty(this, '_value', {writable : true, configurable : true});
-        this._value = value;
-        Object.defineProperty(this, '_value', {writable : false, configurable : false});
+        Object.defineProperty(this, '_state', {writable : false, configurable : false, value : 'fulfilled'});
+        Object.defineProperty(this, '_value', {writable : false, configurable : false, value : value });
+        onfulfilled.call(this);
     };
 
+    const onfulfilledList = new Map();
+
     Object.defineProperties(MyPromise.prototype, {
-        catch : { value : MyPromiseCatch },
-        finally : { value : MyPromiseFinally },
-        then : { value : MyPromiseThen },
-        _constructor : { value : MyPromiseConstructor },
-        _parentPromise : { value : undefined },
-        _genRejectionFunc : { value : genRejectionFunc },
-        _genResolutionFunc : { value : genResolutionFunc },
-        _setRejection : { value : setRejection },
-        _setResolution : { value : setResolution },
-        _state : { value : 'pending', writable : false },
-        _value : { value : undefined, writable : false },
-        _reason : { value : undefined, writable : false },
-        _isTakeOver : { value : false, writable : false }
+        catch : { value : MyPromiseCatch, configurable : false },
+        finally : { value : MyPromiseFinally, configurable : false },
+        then : { value : MyPromiseThen, configurable : false },
+        constructor : { value : MyPromise, configurable : false },
+        _init : { value : MyPromiseConstructor, configurable : false },
+        _parentPromise : { value : undefined, configurable : false },
+        _onfulfilled : { value : onfulfilled, configurable : false },
+        _state : { value : 'pending', configurable : false },
+        _value : { value : undefined, configurable : false },
+        _reason : { value : undefined, configurable : false },
+        _throwTid : { value : undefined, configurable : false },
+        _hasChildren : { value : false, configurable : false }
     });
 }
